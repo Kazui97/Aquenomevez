b4bcd2ceca6b7415239bf74d61921d6d06041592
Kazui97 <camyescobar12@gmail.com>
2019-12-05 14:31:24 -0500

    yea

---
+++ Assets/Clicks.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Clicks : MonoBehaviour
+{
+   public Camera cam;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
