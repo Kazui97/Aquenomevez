c146e9f271434783b16f4967c659b0be35ad9e82
Kazui97 <camyescobar12@gmail.com>
2019-12-09 22:28:18 -0500

    yea

--- Assets/CodigoBusca.cs
+++ Assets/CodigoBusca.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class CodigoBusca : MonoBehaviour
 {
-  [SerializeField]  private float sighRange = 0;
-  [SerializeField]  private Transform PlayerTransform;
-  private Transform _EnemyTransform;
+    [SerializeField] private float sighRange = 0;
+    [SerializeField] private Transform PlayerTransform;
+    [SerializeField] private GameObject warning = null;
+    [SerializeField] private GameObject detect = null;
+    private Transform enemitransform;
 
-   void Awake()
-   {
-       _EnemyTransform = transform;
-   }
+    void Awake()
+    {
+        enemitransform = transform;
+    }
 
     void Start()
     {
@@ -22,41 +24,65 @@ public class CodigoBusca : MonoBehaviour
 
     void Update()
     {
-        IsLookingThePlayer(PlayerTransform.position);
+        IsLookinThePlayer(PlayerTransform.position);
     }
 
-    private bool IsLookingThePlayer(Vector3 playerposition)
+    private float timeOnsight;
+
+    private void Detecplayer(bool IsLookinThePlayer)
     {
-        var displacement = playerposition - _EnemyTransform.position;
-        var distancetoplayer = displacement.magnitude;
-
-        if(!(distancetoplayer <= sighRange)) return false;
-
-        var dot = Vector3.Dot(_EnemyTransform.forward, displacement.normalized);
-
-        if(!(dot >=0.65)) return false;
-
-        var layermask = 1 << 2;
-
-        layermask = -layermask;
-
-        if(Physics.Raycast(_EnemyTransform.position + new Vector3(0,1.5f,0),displacement.normalized, out var hit, sighRange, layermask))
+        warning.SetActive(true);
+        detect.SetActive(false);
+        if(timeOnsight<=3)
         {
-            Debug.DrawRay(_EnemyTransform.position + new Vector3(0, 1.5f, 0), displacement.normalized * hit.distance, Color.red);
-            Debug.Log("Did hit");
+            timeOnsight += Time.deltaTime;
 
+            if (!(timeOnsight >= 3)) return;
+            warning.SetActive(false);
+            detect.SetActive(true);
+            
+        }
+       
 
-            if (hit.collider.GetComponent<prueba>())
+        else
+        {
+            if(timeOnsight > 0)
             {
-                Debug.DrawRay(_EnemyTransform.position + new Vector3(0, 1.5f, 0), displacement.normalized * hit.distance, Color.red);
-                if(!hit.collider.GetComponent<prueba>())
-                {
-                    Debug.Log("yea");
-                }
-                return true;
+                timeOnsight -= Time.deltaTime;
+            }
+            if (!(timeOnsight <= 0)) return;
+            warning.SetActive(false);
+            detect.SetActive(false);
+        }
+        
+    }
+
+    private bool IsLookinThePlayer(Vector3 playerPosition)
+    {
+        var displacement = playerPosition - enemitransform.position;
+        var distacetoPlayer = displacement.magnitude;
+        if (distacetoPlayer <= sighRange)
+        {
+            var dot = Vector3.Dot(enemitransform.forward, displacement.normalized);
+            if (!(dot >= 0.5)) return false;
+
+            var layerMask = 1 << 2;
+
+            layerMask = ~layerMask;
+
+            if(Physics.Raycast(enemitransform.position,displacement.normalized,out var hit , sighRange, layerMask))
+            {
+                Debug.DrawRay(enemitransform.position, displacement.normalized * hit.distance, Color.red);
+
+                Debug.Log("Did hit");
+
+                if (!hit.collider.GetComponent<prueba>()) return false;
+
+                Debug.DrawRay(enemitransform.position, displacement.normalized * hit.distance, Color.green);
+
+                return false;
             }
         }
-
     }
-  
+    
 }
